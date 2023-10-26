3e778ddd6022de4d4ade2ed235d7196924288feb
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-26 09:44:38 -0300

    database

---
+++ fiap.sql
@@ -0,0 +1,20 @@
+        CREATE TABLE T_ALUNO (
+            id_aluno int,
+            rm_aluno varchar2(100)
+        
+        
+        );
+        CREATE TABLE T_ENDERECO (
+        cep int primary key,
+        nm_rua varchar2(100),
+        cidade varchar2(100)
+        )
+drop table T_aluno
+        
+        
+        
+
+--Meu primeiro coment�rio
+/*
+Meu primeiro coment�rio em multiplas linhas
+*/
